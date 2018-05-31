798cea2ae3510a2fb7229f757330560ef097a233
Jubi Taneja <jubi@cs.utah.edu>
2018-05-31 15:34:52 -0700

    small instruction matcher

--- lib/codegen/src/superopt.rs
+++ lib/codegen/src/superopt.rs
@@ -5,21 +5,45 @@ use ir::function::Function;
 use isa::TargetIsa;
 use timing;
 use ir::Inst;
+use ir::InstructionData;
+use ir::instructions::Opcode;
 
 // Optimization driver function that will invoke all
 // the optimization transformations.
 fn opt_driver(pos: &mut FuncCursor, inst: Inst) {
     println!("** Inst Name === {}", pos.func.dfg.display_inst(inst, None));
+    match pos.func.dfg[inst] {
+        InstructionData::Binary { opcode, args } => {
+            let new_opcode = match opcode {
+                Opcode::Iadd => {
+                    println!("Found iadd");
+                },
+                Opcode::Imul => {
+                    println!("Found imul");
+                },
+                _ => {
+                    println!("This binary ppcode is not yet handled");
+                },
+            };
+        },
+        _ => {
+            println!("Instruction Kind not yet handled");
+        },
+    }
 }
 
 // The main superoptimization pass
 pub fn do_superopt(func: &mut Function, isa: &TargetIsa) {
     let _tt = timing::superopt();
     let mut pos = FuncCursor::new(func);
+
+    println!("==============================");
     while let Some(_ebb) = pos.next_ebb() {
         while let Some(inst) = pos.next_inst() {
             // add transformations here
             opt_driver(&mut pos, inst);
         }
     }
+    println!("==============================");
+
 }
