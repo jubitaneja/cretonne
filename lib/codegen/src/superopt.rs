601f578d60f6c6c465e5669f1c400f49e76054ef
Jubi Taneja <jubi@cs.utah.edu>
2018-06-05 15:16:25 -0700

    transformation to replace multiple iadd by imul_imm

--- lib/codegen/src/superopt.rs
+++ lib/codegen/src/superopt.rs
@@ -1,49 +1,62 @@
 // Superoptimization pass
 
 use cursor::{Cursor, FuncCursor};
-use ir::function::Function;
+//use ir::function::Function;
 use isa::TargetIsa;
 use timing;
 use ir::Inst;
-use ir::InstructionData;
+//use ir::InstructionData;
 use ir::instructions::Opcode;
+use ir::dfg::ValueDef;
+use ir::{Function, InstBuilder, InstructionData};
 
 // Optimization driver function that will invoke all
 // the optimization transformations.
+
 fn opt_driver(pos: &mut FuncCursor, inst: Inst) {
-    println!("** Inst Name === {}", pos.func.dfg.display_inst(inst, None));
     match pos.func.dfg[inst] {
         InstructionData::Binary { opcode, args } => {
-            let new_opcode = match opcode {
+            match opcode {
                 Opcode::Iadd => {
-                    println!("Found iadd");
+
+                    // check the type of args
+                    if let ValueDef::Param(_, _) = pos.func.dfg.value_def(args[0]) {
+                        if let ValueDef::Result(arg_ty, _) = pos.func.dfg.value_def(args[1]) {
+                            if let InstructionData::Binary {
+                                opcode: Opcode::Iadd, args,
+                            } = pos.func.dfg[arg_ty]
+                            {
+                                if let ValueDef::Param(_, _) = pos.func.dfg.value_def(args[0]) {
+                                    if let ValueDef::Param(_, _) = pos.func.dfg.value_def(args[1]) {
+                                        pos.func
+                                            .dfg
+                                            .replace(inst)
+                                            .imul_imm(args[1], 3);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        return;
+                    }
                 },
-                Opcode::Imul => {
-                    println!("Found imul");
-                },
-                _ => {
-                    println!("This binary ppcode is not yet handled");
-                },
+                _ => {},
             };
         },
-        _ => {
-            println!("Instruction Kind not yet handled");
-        },
+        _ => {},
     }
 }
 
 // The main superoptimization pass
-pub fn do_superopt(func: &mut Function, isa: &TargetIsa) {
+pub fn do_superopt(func: &mut Function, _isa: &TargetIsa) {
     let _tt = timing::superopt();
     let mut pos = FuncCursor::new(func);
 
-    println!("==============================");
     while let Some(_ebb) = pos.next_ebb() {
         while let Some(inst) = pos.next_inst() {
             // add transformations here
             opt_driver(&mut pos, inst);
         }
     }
-    println!("==============================");
-
 }
